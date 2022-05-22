47cae703de3520f6479225e9e7934213c5ec127f
Viktoria <vikabu2002@mail.ru>
2022-05-22 10:59:13 +0300

    React. Рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,22 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
-export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+export function Effects(props: { sourceId: string; payload: number }) {
+    const [mess, setMess] = useState(1);
+    const callback = (payload: number) => {
+        setMess(payload);
+    };
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+            setMess(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {mess}
+        </div>
+    );
 }
